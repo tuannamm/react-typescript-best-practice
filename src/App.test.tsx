1c035fc56996b5b5e54851d9e54115a03e1b12a0
Hoa Pham <hoapt_1@digi-texx.vn>
2023-01-30 16:47:55 +0700

    update source

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,9 @@
-import { render } from '@testing-library/react';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  expect(1).toBe(1);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
